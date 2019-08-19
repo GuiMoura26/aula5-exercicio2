fb306cb160f9aa7233ce480376151a0a5b1044f2
Guilherme <guilhermemouraa5@gmail.com>
2019-08-19 16:40:34 -0300

    Adicionar exercicio 2

---
+++ exercicio1.py
@@ -0,0 +1,9 @@
+numero = [1,33,24,12,15,74,46,876,22]
+
+def verificar (numero):
+    for n in numero:
+        if n % 2 == 0:
+            print('o numero é par')
+
+        else:
+            print('o numero é impar')
